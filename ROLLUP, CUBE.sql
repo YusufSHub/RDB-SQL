0c3638406fb1488da17b98bfbfabda9ae35902e0
thatzme <yusufizika@gmail.com>
2022-03-10 10:27:46 +0000

    cube

--- ROLLUP, CUBE.sql
+++ ROLLUP, CUBE.sql
@@ -20,3 +20,30 @@ where Category = 'Computer Accessories'
 group by
 	CUBE(Brand, Category, Model_Year)
 order by Brand, Category, Model_Year
+
+
+
+SELECT *
+FROM
+(
+SELECT Category, total_sales_price
+FROM sale.sales_summary
+) A
+PIVOT
+(
+	SUM(total_sales_price)
+	FOR Category
+	IN
+	([Audio & Video Accessories]
+	,[Bluetooth]
+	,[Car Electronics]
+	,[Computer Accessories]
+	,[Earbud]
+	,[gps]
+	,[Hi-Fi Systems]
+	,[Home Theater]
+	,[mp4 player]
+	,[Receivers Amplifiers]
+	,[Speakers]
+	,[Televisions & Accessories])
+) AS PIVOT_TABLE
