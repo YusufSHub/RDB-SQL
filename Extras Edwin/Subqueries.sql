2ef6acdd0801af100e078f4283e8700f67a24485
thatzme <yusufizika@gmail.com>
2022-03-12 16:31:19 +0000

    extra

--- Extras Edwin/Subqueries.sql
+++ Extras Edwin/Subqueries.sql
@@ -1,6 +1,12 @@
 USE [BikeStores]
-
+-- staff that venita daniel manager of 
 select * from sales.staffs
 where manager_id =(
 select staff_id from sales.staffs
 where first_name='Venita' and last_name='Daniel')
+
+select A.*
+from sales.staffs A, sales.staffs B
+where A.manager_id = B.staff_id and
+B.first_name='Venita' and B.last_name='Daniel'
+
