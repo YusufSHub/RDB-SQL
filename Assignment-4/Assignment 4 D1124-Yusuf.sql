ef302b6db7ba5e28deac583c3ee19c8f7345206c
thatzme <yusufizika@gmail.com>
2022-03-16 17:02:03 +0000

    updates

--- Assignment-4/Assignment 4 D1124-Yusuf.sql
+++ Assignment-4/Assignment 4 D1124-Yusuf.sql
@@ -8,19 +8,10 @@ In this assignment, you are expected to generate a solution
 using SQL with a logical approach. */
 
 select DISTINCT a.product_id, b.discount,
-sum(b.quantity) over(partition by a.product_id, b.discount) total_per_discount
+sum(b.quantity) over(partition by a.product_id, b.discount) total_per_discount,
+sum(b.quantity) over(partition by a.product_id) total_per_product,
+avg(b.quantity) over(partition by a.product_id, b.discount),
+cast((sum(b.quantity) over(partition by a.product_id, b.discount)* 1.0/(sum(b.quantity) over(partition by a.product_id)))as numeric(3,2)) percentage
 from product.product a, sale.order_item b, sale.orders c
 where a.product_id=b.product_id and b.order_id=c.order_id
 order by a.product_id,b.discount
-
-
-select a.product_id, b.discount
-from product.product a, sale.order_item b
-where a.product_id=b.product_id 
-order by a.product_id,b.discount
-
-select * from product.product
-
-select * from sale.order_item
-where product_id=1
-order by product_id
