b0da153675cfa6b2a6ad4e1cc1666c1874e88fe9
thatzme <yusufizika@gmail.com>
2022-03-21 21:21:16 +0000

    project

--- Assignment-5/Assignment 5 D1124-Yusuf.sql
+++ Assignment-5/Assignment 5 D1124-Yusuf.sql
@@ -2,3 +2,25 @@
 Create a scalar-valued function that returns the factorial of a number you gave it.*/
 
 
+CREATE FUNCTION factorial
+(@mynumber INT)
+
+RETURNS INT
+
+BEGIN
+ declare @counter INT, @total INT
+
+	SET @counter = 1
+	SET @total = 1
+
+	while @counter <= @mynumber
+		BEGIN
+		set @total = @total* @counter
+		set @counter = @counter + 1
+		END;
+    RETURN @total
+END;
+
+print dbo.factorial(5)
+
+select dbo.factorial(5)
--- RDB&SQL - E Commerce Data Analysis Project/E-Commerce Project D1124-Yusuf.sql
+++ RDB&SQL - E Commerce Data Analysis Project/E-Commerce Project D1124-Yusuf.sql
@@ -1,4 +1,4 @@
-
+Ôªøuse [E Commerce Project]
 --1. Join all the tables and create a new table called combined_table. 
 --(market_fact, cust_dimen, orders_dimen, prod_dimen, shipping_dimen)
 
@@ -12,7 +12,7 @@ select *
 into combined_table 
 from combined_view
 
-
+select * from combined_view
 
 
 
@@ -86,19 +86,47 @@ SELECT Cust_id, Customer_Name, Order_Date, Ship_Date, DaysTakenForDeliveryFROM 
 --month over the entire year in 2011
 --You can use date functions and subqueries
 
+select MONTH(Order_Date) Month_Num, DATENAME(MONTH, Order_Date) Monthz, 
+	COUNT(DISTINCT Cust_id) Cust_Count from combined_table
+	where Cust_id in ( 
+					select Cust_id from combined_table
+					where MONTH(Order_Date)=1 and YEAR(Order_Date)=2011
+					) 
+	and YEAR(Order_Date)=2011
+group by MONTH(Order_Date), DATENAME(MONTH, Order_Date)
+order by 1
 
 
-
+	SELECT MONTH(Order_Date) Month_No, COUNT(DISTINCT cust_id) MONTHLY_NUM_OF_CUST
+		FROM combined_table A --exists te i√ßerdeki query ile dƒ±≈üardakini baƒülamam gerektiƒüi i√ßin A dedim.
+		WHERE
+		EXISTS
+		(
+			SELECT cust_id
+			FROM combined_table B
+			WHERE YEAR(Order_Date) = 2011
+			AND MONTH(Order_Date) = 1
+			AND A.Cust_id = B.Cust_id
+		)
+		AND YEAR(Order_Date) = 2011
+	GROUP BY MONTH(order_date)
 
 --////////////////////////////////////////////
-
 
 --6. write a query to return for each user acording to the time elapsed between the first purchasing and the third purchasing, 
 --in ascending order by Customer ID
 --Use "MIN" with Window Functions
 
-
-
+select DISTINCT Cust_id, Order_Date [third_order_date], first_order_date, third_order,
+	DATEDIFF(Day, first_order_date, order_date) Date_difference
+from 
+ (
+	select DISTINCT Cust_id, Order_Date,	
+	 min(Order_date) over(Partition by Cust_id) first_order_date,
+	 DENSE_RANK() over(Partition by Cust_id order by order_date) third_order
+	from combined_table
+	) T
+where third_order=3
 
 
 --//////////////////////////////////////
@@ -120,18 +148,31 @@ with tablo1 as (
 ) 
  select cust_id, 
 	count_prod11,
-	cast((1.0*count_prod11/total_product) as decimal (3,2)) [ratio_p11], 
+	cast((1.0*count_prod11/total_product)*100 as decimal (5,2)) [ratio_p11], 
 	count_prod14,
-	cast((1.0*count_prod14/total_product) as decimal (3,2)) [ratio_p14] ,
+	cast((1.0*count_prod14/total_product)*100 as decimal (5,2)) [ratio_p14] ,
 	total_product
  from tablo1 
  order by ratio_p14 DESC, ratio_p11 ASC
 
+
+
+ select cust_id,
+	   SUM(CASE WHEN Prod_id='Prod_11' THEN order_quantity ELSE 0 end) Prod_11_count,
+	   SUM(CASE WHEN Prod_id='Prod_14' THEN order_quantity ELSE 0 end) Prod_14_count,
+	  (SUM(CASE WHEN Prod_id='Prod_11' THEN order_quantity ELSE 0 end) + SUM(CASE WHEN Prod_id='Prod_14' THEN order_quantity ELSE 0 end))* 1.0 / SUM (Order_Quantity)*100  as Perc
+FROM combined_table
+where Cust_id in (
+				SELECT Cust_id
+					 FROM combined_table
+					 WHERE Prod_id IN ('Prod_11', 'Prod_14')
+					 GROUP BY Cust_id
+					 HAVING COUNT(DISTINCT Prod_id)=2
+				)
+GROUP BY Cust_id
+order by Cust_id
 --/////////////////
 
-
-select * from combined_table where  Cust_id='Cust_1538'
-select * from combined_table where Cust_id='Cust_1538'
 
 --CUSTOMER SEGMENTATION
 
@@ -140,16 +181,31 @@ select * from combined_table where Cust_id='Cust_1538'
 --1. Create a view that keeps visit logs of customers on a monthly basis. (For each log, three field is kept: Cust_id, Year, Month)
 --Use such date functions. Don't forget to call up columns you might need later.
 
+CREATE VIEW Customer_Log as
+select cust_id, DATENAME(YEAR, order_date) Year_Visit, DATENAME(MONTH, Order_Date) Month_Visit
+from combined_table
+group by Cust_id, DATENAME(YEAR, order_date), DATENAME(MONTH, Order_Date)
 
 
+
+select * from combined_table
 --//////////////////////////////////
 
 
 
-  --2.Create a ìviewî that keeps the number of monthly visits by users. (Show separately all months from the beginning  business)
+  --2.Create a ‚Äúview‚Äù that keeps the number of monthly visits by users. (Show separately all months from the beginning  business)
 --Don't forget to call up columns you might need later.
 
+CREATE VIEW Monthly_Visit as 
+select DATENAME(MONTH, order_date) Month_Visit, count(DATENAME(MONTH, order_date)) Visit_Count
+from combined_table
+group by DATENAME(MONTH, order_date)
 
+select * from Monthly_Visit
+
+
+select * from combined_table 
+where DATENAME(MONTH, order_date)='February'
 
 
 
@@ -161,7 +217,28 @@ select * from combined_table where Cust_id='Cust_1538'
 --then create a new column for each month showing the next month using the order you have made above. (use "LEAD" function.)
 --Don't forget to call up columns you might need later.
 
+select Cust_id, Order_Date, second_order 
+from
+	(select DISTINCT Cust_id, Order_Date,	
+	 min(Order_date) over(Partition by Cust_id) first_order_date,	 
+	 lead(Order_Date, 1) over(partition by cust_id order by order_date) second_order,
+	 DENSE_RANK() over(Partition by Cust_id order by order_date) order_datez
+	 from combined_table
+	 ) T
+	
+select Cust_id, Order_Date, second_order 
+from
+	(select DISTINCT Cust_id, Order_Date,	
+	 min(Order_date) over(Partition by Cust_id) first_order_date,	 
+	 lead(Order_Date, 1) over(partition by cust_id order by order_date) second_order,
+	 DENSE_RANK() over(Partition by Cust_id order by order_date) order_datez
+	 from combined_table
+	 ) T
+	 where DATEDIFF(MONTH,Order_Date,second_order)=1
+	
 
+
+select * from Customer_Log
 
 --/////////////////////////////////
 
@@ -170,10 +247,14 @@ select * from combined_table where Cust_id='Cust_1538'
 --4. Calculate monthly time gap between two consecutive visits by each customer.
 --Don't forget to call up columns you might need later.
 
-
-
-
-
+select Cust_id, order_date, second_order, DATEDIFF(MONTH, order_date, second_order) Month_Gap
+from (
+	select DISTINCT Cust_id, Order_Date,	
+	 min(Order_date) over(Partition by Cust_id) first_order_date,	 
+	 lead(Order_Date,1) over(partition by cust_id order by order_date) second_order
+	 from combined_table
+	 ) T
+where DATEDIFF(MONTH, order_date, second_order) > 0
 
 
 --///////////////////////////////////
@@ -181,23 +262,36 @@ select * from combined_table where Cust_id='Cust_1538'
 
 --5.Categorise customers using average time gaps. Choose the most fitted labeling model for you.
 --For example: 
---Labeled as ìchurnî if the customer hasn't made another purchase for the months since they made their first purchase.
---Labeled as ìregularî if the customer has made a purchase every month.
+--Labeled as ‚Äúchurn‚Äù if the customer hasn't made another purchase for the months since they made their first purchase.
+--Labeled as ‚Äúregular‚Äù if the customer has made a purchase every month.
 --Etc.
 	
+with T1 as(
+select Cust_id, DATEDIFF(MONTH, order_date, second_order) Month_Gap
+from (
+	select DISTINCT Cust_id, Order_Date,	
+	 min(Order_date) over(Partition by Cust_id) first_order_date,	 
+	 lead(Order_Date,1) over(partition by cust_id order by order_date) second_order
+	 from combined_table
+	 ) T
+)
+
+SELECT cust_id, AVG(t1.month_gap) AS AvgTimeGap,       CASE WHEN AVG(t1.month_gap) IS NULL THEN 'Churn'	     WHEN MAX(t1.month_gap) = 1 THEN 'regular'	     ELSE 'irregular'	       END CustLabelsFROM T1GROUP BY cust_id
+order by CustLabels
 
 
 
 
 
-
+select * from combined_table
+where Cust_id='Cust_1049'
 
 --/////////////////////////////////////
 
 
 
 
---MONTH-WISE RETENT›ON RATE
+--MONTH-WISE RETENTION RATE
 
 
 --Find month-by-month customer retention rate  since the start of the business.
@@ -206,9 +300,7 @@ select * from combined_table where Cust_id='Cust_1538'
 --1. Find the number of customers retained month-wise. (You can use time gaps)
 --Use Time Gaps
 
-
-
-
+SELECT DISTINCT YEAR(order_date) [year],                 MONTH(order_date) [month],                DATENAME(month,order_date) [month_name],                COUNT(cust_id) OVER (PARTITION BY year(order_date), month(order_date) order by year(order_date), month(order_date)  ) num_custFROM combined_table
 
 --//////////////////////
 
@@ -222,9 +314,15 @@ select * from combined_table where Cust_id='Cust_1538'
 
 --You should pay attention to the join type and join columns between your views or tables.
 
+CREATE VIEW TA as
+(
+	SELECT DISTINCT YEAR(order_date) [year],                 MONTH(order_date) [month],                DATENAME(month,order_date) [month_name],                COUNT(cust_id) OVER (PARTITION BY year(order_date), month(order_date) order by year(order_date), month(order_date)  ) num_cust	FROM combined_table
+	) 
 
-
-
+select year, month, num_cust, lead(num_cust,1) over (order by year, month) rate_,
+	FORMAT(num_cust*1.0*100/(lead(num_cust,1) over (order by year, month, num_cust)), 'N1') Retention_rate
+from TA
+order by year, month, num_cust
 
 
 
